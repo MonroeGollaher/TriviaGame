774132a5d05464340dbcf724523538b2c8bb4d4f
tompasini <pasinitom91@gmail.com>
2020-11-18 11:48:40 -0700

    recommit

--- triviagamecapstone.server/server/models/Game.js
+++ triviagamecapstone.server/server/models/Game.js
@@ -1,11 +1,6 @@
-<<<<<<< HEAD
 function myFunction(str1){
   return str1.split('')
 
-=======
-function myFunction(str) {
-  return str.split('')
->>>>>>> 0bfd21ae5218ffe56590bacaa305a040212ffaaa
 }
 
 myFunction('Hello World')
