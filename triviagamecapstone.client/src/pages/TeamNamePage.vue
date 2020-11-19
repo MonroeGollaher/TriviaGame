f7a0c1847a39ba8f4325bfe62a4d7e2925fe691d
Ryan Carpenter <rccarpenter@outlook.com>
2020-11-19 10:22:59 -0700

    build team views (mobile first)

---
+++ triviagamecapstone.client/src/pages/TeamNamePage.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="TeamName container-fluid">
+    <div class="row justify-content-center align-items-center">
+      <div class="col-12 col-lg-6">
+        <h3>
+          Set Team Name
+        </h3>
+      </div>
+    </div>
+    <div class="row justify-content-center align-items-center">
+      <div class="col-12 col-lg-6">
+        <form @submit="setTeamName()">
+          <input type="text" v-model="state.team.name">
+          <button type="submit" class="btn btn-success">
+            Play
+          </button>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { reactive } from 'vue'
+export default {
+  name: 'TeamName',
+  setup() {
+    const state = reactive({
+      team: {}
+    })
+    return {
+      state
+    }
+  },
+  components: {}
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ triviagamecapstone.client/src/pages/TeamProfilePage.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="TeamProfile container-fluid">
+    <div class="row justify-content-center">
+      <div class="col-12 col-lg-6 d-flex flex-column align-items-center">
+        <img src="//placehold.it/200" alt="team picture" class="border-circle">
+        <h2>
+          team.name
+        </h2>
+      </div>
+    </div>
+    <div class="row justify-content-center">
+      <div class="col-12 col-lg-6">
+        <!-- NOTE - League Leaderboard component -->
+      </div>
+    </div>
+    <div class="row justify-content-center">
+      <div class="col-12 col-lg-6 d-flex flex-column align-items-center">
+        <h5>Team Members</h5>
+        <form @submit.prevent="addTeamMember()">
+          <input type="text" v-model="state.newMember">
+          <button type="submit">
+            <i class="far fa-plus-square"></i>
+          </button>
+        </form>
+        <ul>
+          <li>
+            team.memberName
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { reactive } from 'vue'
+export default {
+  name: 'TeamProfile',
+  // props: {
+  //   teamProp: Object
+  // },
+  setup() {
+    const state = reactive({
+      newMember: {}
+    })
+    return {
+      state
+      // team: computed(() => props.teamProp)
+    }
+  },
+  components: {}
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
