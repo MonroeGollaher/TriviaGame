4ad3fff466cb7cab82174c66e507613b77ef463d
Ryan Carpenter <rccarpenter@outlook.com>
2020-11-19 10:29:01 -0700

    leaderboard components

---
+++ triviagamecapstone.client/src/components/GameLeaderBoardComponent.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="GameLeaderBoard container-fluid">
+    <div class="row justify-content-center align-items-center">
+      <div class="col-12 col-lg-6 d-flex">
+        <p>TeamName</p>
+        <p class="ml-2">
+          TeamRank
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'GameLeaderBoard',
+  setup() {
+    return {}
+  },
+  components: {}
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
