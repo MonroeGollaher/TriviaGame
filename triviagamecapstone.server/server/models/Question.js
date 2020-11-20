29a41c9d2d6795e3b432e1172b9a88bf5e2e5878
Michelle Charron <michelle.charron93@gmail.com>
2020-11-20 14:24:19 -0700

    Built out basics for question and responses

---
+++ triviagamecapstone.server/server/models/Question.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose'
+const Schema = mongoose.Schema
+const ObjectId = Schema.Types.ObjectId
+
+const Question = new Schema(
+  {
+    category: { type: String, required: true },
+    question: { type: String, required: true },
+    answer: { type: String, required: true },
+    gameId: { type: ObjectId, ref: 'Game', required: true }
+  },
+  { timestamps: true, _id: true, toJSON: { virtuals: true } }
+)
+
+export default Question
