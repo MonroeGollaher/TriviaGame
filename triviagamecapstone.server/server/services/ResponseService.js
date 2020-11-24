eb9c3cdd1a27e9e72f7705e21b05a167135ba45c
tompasini <pasinitom91@gmail.com>
2020-11-23 17:09:18 -0700

    increment and decrement wager by approval

--- triviagamecapstone.server/server/services/ResponseService.js
+++ triviagamecapstone.server/server/services/ResponseService.js
@@ -3,10 +3,33 @@ import { BadRequest } from '../utils/Errors'
 
 class ResponseService {
   async toggleApproval(body, responseId, userInfo) {
+    const response = await dbContext.Responses.findOne({ _id: responseId })
+    // @ts-ignore
+    const wager = response._doc.wager
     if (userInfo.roles[0] === 'Host') {
-      return await dbContext.Responses.findByIdAndUpdate(responseId, body, { new: true })
+      const res = await dbContext.Responses.findByIdAndUpdate(responseId, body, { new: true })
+      this.updatePoints(userInfo.id, wager, body.approved)
+      return res
     } else {
       throw new BadRequest('Permission denied')
+    }
+  }
+
+  async updatePoints(profileId, wager, approvedValue) {
+    const profile = await dbContext.Profile.findById(profileId)
+    let currentValue = profile._doc.currentPoints
+    if (approvedValue === true) {
+      currentValue += wager
+      const reqBodyInc = {
+        currentPoints: currentValue
+      }
+      await dbContext.Profile.findByIdAndUpdate(profileId, reqBodyInc)
+    } else if (approvedValue === false) {
+      currentValue -= wager
+      const reqBodyDec = {
+        currentPoints: currentValue
+      }
+      await dbContext.Profile.findByIdAndUpdate(profileId, reqBodyDec)
     }
   }
 
